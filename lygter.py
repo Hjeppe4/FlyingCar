7f07904381ce274c2314662ab0ee6f124794752b
jeppe <hjeppe4@gmail.com>
2023-10-12 11:03:18 +0200

    LUL

--- lygter.py
+++ lygter.py
@@ -6,11 +6,11 @@ wiringpi.wiringPiSetupGpio()
 light1 = 14
 light2 = 15
 
-wiringpi.softPwmCreate(light1, 0, 180) 
+wiringpi.softPwmCreate(light1, 0, 100) 
 wiringpi.softPwmCreate(light2, 0, 100)
 
-wiringpi.softPwmWrite(light1, 180)
-wiringpi.softPwmWrite(light2, 180)
+wiringpi.softPwmWrite(light1, 100)
+wiringpi.softPwmWrite(light2, 100)
 time.sleep(3)
 wiringpi.softPwmWrite(light1, 0)
 wiringpi.softPwmWrite(light2, 0)
