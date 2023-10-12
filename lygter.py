7d735bd9215bde9b3cbee8d7fb93507de0a4c67f
jeppe <hjeppe4@gmail.com>
2023-10-12 11:04:32 +0200

    dasdas

--- lygter.py
+++ lygter.py
@@ -8,9 +8,12 @@ light2 = 15
 
 wiringpi.softPwmCreate(light1, 0, 100) 
 wiringpi.softPwmCreate(light2, 0, 100)
+print("test")
 
 wiringpi.softPwmWrite(light1, 100)
 wiringpi.softPwmWrite(light2, 100)
+print("test  2")
 time.sleep(3)
 wiringpi.softPwmWrite(light1, 0)
 wiringpi.softPwmWrite(light2, 0)
+print ("test 3")
