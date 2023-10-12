16d458a5174c648db9a3af21f05a7f22e5b1cd05
jeppe <hjeppe4@gmail.com>
2023-10-12 11:39:09 +0200

    kode

--- lygter.py
+++ lygter.py
@@ -1,16 +1,19 @@
-from gpiozero import Servo
-from time import sleep
+# Set up libraries and overall settings
+import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
+from time import sleep   # Imports sleep (aka wait or pause) into the program
+GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
 
-servo = Servo(32)
+# Set up pin 11 for PWM
+GPIO.setup(12,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
+p = GPIO.PWM(12, 50)     # Sets up pin 11 as a PWM pin
+p.start(0)               # Starts running PWM on the pin and sets it to 0
 
-try:
-	while True:
-    	servo.min()
-    	sleep(0.5)
-    	servo.mid()
-    	sleep(0.5)
-    	servo.max()
-    	sleep(0.5)
+# Move the servo back and forth
+p.ChangeDutyCycle(3)     # Changes the pulse width to 3 (so moves the servo)
+sleep(1)                 # Wait 1 second
+p.ChangeDutyCycle(12)    # Changes the pulse width to 12 (so moves the servo)
+sleep(1)
 
-except KeyboardInterrupt:
-	print("Program stopped")
+# Clean up everything
+p.stop()                 # At the end of the program, stop the PWM
+GPIO.cleanup()           # Resets the GPIO pins back to defaults
