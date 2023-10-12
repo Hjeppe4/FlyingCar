93b65eeca73613160f1b115ee303168c79756e1c
jeppe <hjeppe4@gmail.com>
2023-10-12 10:59:57 +0200

    ja tak

--- lygter.py
+++ lygter.py
@@ -6,7 +6,7 @@ light1 = 14
 light2 = 15
 
 wiringpi.softPwmCreate(light1, 0, 180) 
-wiringpi.softPwmCreate(light2, 0, 180 
+wiringpi.softPwmCreate(light2, 0, 180)
 
 wiringpi.softPwmWrite(light1, 180)
 wiringpi.softPwmWrite(light2, 180)
